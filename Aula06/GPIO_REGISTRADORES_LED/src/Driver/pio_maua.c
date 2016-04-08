805a930b3759201948b5a85ebad40795543e611e
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2016-04-07 21:36:12 -0300

    implentando output

--- Aula06/GPIO_REGISTRADORES_LED/src/Driver/pio_maua.c
+++ Aula06/GPIO_REGISTRADORES_LED/src/Driver/pio_maua.c
@@ -16,12 +16,29 @@ void _pio_set_output(	Pio *p_pio,
                 const uint32_t ul_mask,
 		       	const uint32_t ul_default_level,
 		       	const uint32_t ul_pull_up_enable){
-					   p_pio->PIO_PER = (ul_mask & ul_pull_up_enable); //Habilita MUX do buffer de saída
-					   p_pio->PIO_OER = (ul_mask & ul_pull_up_enable); //Habilita Buffer de saída
-					   p_pio->PIO_MDER = (ul_mask & ul_default_level); //Habilita MUX para escrita
-					   p_pio->PIO_PER = (ul_mask & ul_default_level); //Habilita MUX para escrita
-					   p_pio->PIO_SODR = (ul_mask & ul_default_level); //Seta valores no pinos					   
-		}
+					   
+					   
+	_pio_pull_up(p_pio, ul_mask, ul_pull_up_enable);			   
+	
+	if(ul_default_level)
+		_pio_set(p_pio,ul_mask);
+	else
+		_pio_clear(p_pio,ul_mask);
+		
+	p_pio->PIO_PER = (ul_mask); //Habilita MUX do buffer de saída
+	p_pio->PIO_OER = (ul_mask); //Habilita Buffer de saída	
+	
+	
+	/*				   
+
+	p_pio->PIO_MDER = (ul_mask & ul_default_level); //Habilita MUX para escrita
+	p_pio->PIO_PER = (ul_mask & ul_default_level); //Habilita MUX para escrita
+	p_pio->PIO_SODR = (ul_mask & ul_default_level); //Seta valores no pinos					   
+	*/
+	
+	
+	
+}
 
 
 
@@ -35,8 +52,7 @@ void _pio_set_output(	Pio *p_pio,
  * \param ul_mask Bitmask indicating which pin(s) to configure as input(s).
  * \param ul_attribute PIO attribute(s).
  */
-void _pio_set_input( 	Pio *p_pio, 
-                    	const uint32_t ul_mask,
+void _pio_set_input( 	Pio *p_pio,           	const uint32_t ul_mask,
             	   	const uint32_t ul_attribute)
 					   {
 						   p_pio->PIO_ODR = (ul_mask & ul_attribute);
@@ -51,11 +67,14 @@ void _pio_set_input( 	Pio *p_pio,
  * \param ul_pull_up_enable Indicates if the pin(s) internal pull-up shall be
  * configured.
  */
-void _pio_pull_up(	Pio *p_pio, 
-               const uint32_t ul_mask,
-		            const uint32_t ul_pull_up_enable){
-					p_pio->PIO_PUER = (ul_mask & ul_pull_up_enable);	
-					}
+void _pio_pull_up(	Pio *p_pio,  const uint32_t ul_mask, const uint32_t ul_pull_up_enable){
+	
+	if(ul_pull_up_enable)
+		p_pio->PIO_PUER = ul_mask;
+	else
+		p_pio->PIO_PUDR = ul_mask;
+}
+
 
 /**
  * \brief Configure PIO pin internal pull-down.
@@ -81,7 +100,11 @@ void _pio_pull_down( Pio *p_pio,
  * \param ul_mask Bitmask of one or more pin(s) to configure.
  */
 void _pio_set(   Pio *p_pio, 
-                const uint32_t ul_mask);
+                const uint32_t ul_mask){
+					
+	p_pio->PIO_SODR = ul_mask;
+					
+}
 
 /**
  * \brief Set a low output level on all the PIOs defined in ul_mask.
@@ -92,7 +115,11 @@ void _pio_set(   Pio *p_pio,
  * \param ul_mask Bitmask of one or more pin(s) to configure.
  */
 void _pio_clear( Pio *p_pio, 
-                const uint32_t ul_mask); 
+                const uint32_t ul_mask)
+{
+	p_pio->PIO_SCDR = ul_mask;
+	
+}
 
 
 /**
